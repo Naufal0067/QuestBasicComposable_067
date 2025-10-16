427df102861be35aa1f5e8f55b2ae657f7b7bf81
Naufal0067 <naufalopang1928@gmail.com>
2025-10-16 19:38:50 +0700

    menambahkan fungsi tataletakcolumnrow

--- app/src/main/java/com/example/tataletak/MainActivity.kt
+++ app/src/main/java/com/example/tataletak/MainActivity.kt
@@ -79,3 +79,7 @@ fun TataletakColumnRow(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataletakRowColumn(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
