18baf16bda387f3b42454bc851448fee6f21ddfb
Naufal0067 <naufalopang1928@gmail.com>
2025-10-16 19:46:15 +0700

    menambahkan fungsi tatalletakboxcolumnrow

--- app/src/main/java/com/example/tataletak/MainActivity.kt
+++ app/src/main/java/com/example/tataletak/MainActivity.kt
@@ -97,3 +97,7 @@ fun TataletakRowColumn(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.notasibalok)
